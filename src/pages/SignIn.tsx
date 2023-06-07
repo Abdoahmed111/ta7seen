4b3d1b1d279846f3071b58ed8704fbf40233cd80
mac <mac@Abdelrahman.local>
2023-06-07 05:39:54 +0300

    enhancements

--- src/pages/SignIn.tsx
+++
@@ -1,9 +0,0 @@
-import { SignedIn } from "@clerk/nextjs";
-import { type NextPage } from "next";
-import React from "react";
-
-const SignIn: NextPage = () => {
-  return <SignedIn />;
-};
-
-export default SignIn;
